dd04be5fb627225ab318624bc34946ddfcaa1ed8
Alora Greer <agreer5@alaska.edu>
2023-08-25 14:41:14 -0800

    add env files for prod and local data pts

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Chart } from 'chart.js';
+import { environment } from 'src/environments/environments.prod';
 
 @Component({
   selector: 'app-line-chart',
@@ -36,7 +37,7 @@ export class LineChartComponent implements OnInit {
   }
 
   fetchData() {
-    this.http.get<any[]>('http://localhost:3000/data').subscribe(data => {
+    this.http.get<any[]>('${environment.apiUrl}/data').subscribe(data => {
       const labels = data.map(row => row.someField);  // Replace 'someField' with the actual field name
       const values = data.map(row => row.anotherField);  // Replace 'anotherField' with the actual field name
 
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://my-service-jfaw5pyhgq-wl.a.run.app/data'  // Your production API URL here
+  };
+  
