213ae3bb72475200a7168f0c659b4326b09483a9
Alora Greer <agreer5@alaska.edu>
2023-08-25 13:24:24 -0800

    add line graph

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LineChartComponent } from './line-chart/line-chart.component';  // Import your component
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'line-chart', component: LineChartComponent }, // Define the route
+  // other routes can go here
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
